861479ac5e2272452e4ee0458b76bc26d83a0fb3
Alwin Thomas <alwnthomas@gmail.com>
2024-11-02 09:41:14 +0000

    Added a toggle dark-mode functionality

--- main.js
+++ main.js
@@ -24,3 +24,17 @@ window.addEventListener("scroll", () => {
     
     lastScrollY = window.scrollY;
 });
+
+// Dark mode toggle button
+const toggleButton = document.getElementById('dark-mode-toggle');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    // Change icon between sun and moon
+    const icon = toggleButton.querySelector('i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.replace('fa-moon', 'fa-sun');
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon');
+    }
+});
